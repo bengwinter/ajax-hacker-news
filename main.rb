e012c172b0042bdc61293f9a19ab35f72312fb5f
Ben Winter <ben.g.winter@gmail.com>
2013-11-06 21:16:42 -0500

    working through post id shit

--- main.rb
+++ main.rb
@@ -17,13 +17,19 @@ class Comment < ActiveRecord::Base
 end
 
 get '/posts.json' do
-  Post.all.to_json 
+  Post.all.to_json
 end
 
 post '/posts/create' do
   Post.create(title: params[:title], link: params[:link], body: params[:body]).to_json
 end
 
-get '/posts/comments.json' do
-  Post.find(params[:id]).comments
+get '/posts/:id/comments.json' do
+  Post.find(params[:id]).comments.to_json
 end
+
+post '/posts/:id/comments.json' do
+  Comment.create(author: params[:author], body: params[:body], post_id: params[:id]).to_json
+end
+
+ $('#posts').append("<div class='post'> <div class='post-title'>" + data.title + "</div> <div class='post-link'>" + data.link + "</div> <div class='post-body'>" + data.body + "</div> <form class='comment-form'> <input name='content' class='comment-content-field'/> <button class='comment-create-button'>Submit</button> </form> </div>");
--- public/main.js
+++ public/main.js
@@ -8,7 +8,7 @@ Hacker.getPosts = function() {
       Hacker.renderPosts(data);
     }
   });
-}
+};
 
 Hacker.createPost = function() {
   var title = $('#post-title-field').val();
@@ -21,6 +21,7 @@ Hacker.createPost = function() {
     url: '/posts/create', 
     type: 'post',
     data: {title: title, link: link, body: body},
+    debugger;
     dataType: 'json',
     success: function(data) {
       $('#posts').append("<div class='post'> <div class='post-title'>" + data.title + "</div> <div class='post-link'>" + data.link + "</div> <div class='post-body'>" + data.body + "</div> <form class='comment-form'> <input name='content' class='comment-content-field'/> <button class='comment-create-button'>Submit</button> </form> </div>");
@@ -34,6 +35,46 @@ Hacker.renderPosts = function(data) {
     $('#posts').append("<div class='post'> <div class='post-title'>" + data.title + "</div> <div class='post-link'>" + data.link + "</div> <div class='post-body'>" + data.body + "</div> <form class='comment-form'> <input name='content' class='comment-content-field'/> <button class='comment-create-button'>Submit</button> </form> </div>");
   });
 };
+
+/////////////////
+
+Hacker.getComments = function() {
+  $.ajax({
+    url: '/posts/:id/comments.json',
+    dataType: 'json',
+    success: function(data) {
+      Hacker.renderComments(data);
+    }
+  });
+}
+
+Hacker.createComment = function() {
+  var content = $('#comment-content-field').val();
+  $('#comment-content-field').val('');
+  $.ajax({
+    url: '/comments/create', 
+    type: 'post',
+    data: {content: content},
+    dataType: 'json',
+    success: function(data) {
+      $('#comments').append("<div class='comment'>" + data.content + "</div>");
+    }
+  });
+};
+
+
+Hacker.renderComments = function(data) {
+  $(data).each(function(index, comment) {
+    $('#comments').append("<div class='comment'>" + comment.content + "</div>");
+  });
+};
+
+$(document).ready(function() {
+  Hacker.getComments();
+  $('#comment-create-button').click(function(e) {
+    e.preventDefault();
+    Blog.createComment();
+    })
 
 $(document).ready(function() {
   Hacker.getPosts();
